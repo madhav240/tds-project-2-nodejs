c34f11e838a0c10b8700238f35ab04e7f5135bdc
Madhav Mishra <madhavmishra1124@gmail.com>
2025-04-01 00:23:44 +0530

    initial commit

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const prettier = require("prettier");
 const app = express();
 const PORT = 4000;
 
+// Middleware to parse JSON
+app.use(express.json());
+
 app.post("/api/prettier", async (req, res) => {
   try {
     if (req.method !== "POST") {
